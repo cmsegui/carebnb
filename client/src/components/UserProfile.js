a8f4ffddde1897d4cadb3c6f793ef5a4e93beb72
cmsegui <cmsegui@yahoo.com>
2017-09-26 18:23:47 -0400

    updated readme and styling

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -23,7 +23,8 @@ class UserProfile extends Component {
   }
   _getUserProfileData = () => {
     const id = this.props.match.params.id;
-    axios.get(`/api/user/${id}`)
+    axios
+      .get(`/api/user/${id}`)
       .then(res => {
         this.setState({ user: res.data });
         console.log(res.data);
@@ -36,10 +37,11 @@ class UserProfile extends Component {
   _deleteHome = (homeId, e) => {
     e.preventDefault();
     const id = this.props.match.params.id;
-    axios.delete(`/api/user/${id}/home/${homeId}`)
+    axios
+      .delete(`/api/user/${id}/home/${homeId}`)
       .then(res => {
-        this._getUserProfileData()
-          alert('Home Deleted');
+        this._getUserProfileData();
+        alert('Home Deleted');
       })
       .catch(err => {
         console.log(err);
@@ -48,60 +50,76 @@ class UserProfile extends Component {
 
   render() {
     if (this.state.redirect) {
-      <Redirect to={`/user/${this.props.match.params.id}`} />
-    } 
-    else {
+      <Redirect to={`/user/${this.props.match.params.id}`} />;
+    } else {
       return (
-      <div>
         <div>
-          <h1>Hi, {this.state.user.username}!</h1>
-        </div>
-        <div>
-          <h3 className="inline-header">These are your homes: </h3>
-          <button type="button" className="btn btn-outline-primary add-button">
-          <Link
-            to={`/user/${this.props.match.params.id}/addHome`}
-            className="btntxtclr">
-            ADD HOME
-          </Link>
-        </button>
-          {this.state.user.homes.map(home => {
-            let mapslink = `http://www.google.com/maps/place/${home.address.latitude},${home.address.longitude}`;
-            return (
-              <div key={home._id} className="clearfix">
+          <div>
+            <h1>Hi, {this.state.user.username}!</h1>
+          </div>
+          <div>
+            <h3 className="inline-header">These are your homes: </h3>
+            <button
+              type="button"
+              className="btn btn-outline-primary add-button"
+            >
+              <Link
+                to={`/user/${this.props.match.params.id}/addHome`}
+                className="btntxtclr"
+              >
+                ADD HOME
+              </Link>
+            </button>
+            {this.state.user.homes.map(home => {
+              let mapslink = `http://www.google.com/maps/place/${home.address
+                .latitude},${home.address.longitude}`;
+              return (
+                <div key={home._id} className="clearfix">
                   <div className="home">
-                      <Link to={`/search/${home._id}`}>
-                        <img src={home.img} alt="homepic" className="imgformat"/>
-                      </Link>
-                  <div className="addy">
-                    <div>{home.address.addressLine1}</div>
-                    <div>{home.address.addressLine2}</div>
-                    <div>{home.address.city}</div>
-                    <div>{home.address.state}</div>
-                    <div>{home.address.zipcode}</div>
-                    <a href={mapslink} target="_maps">View on the Googles</a>
-                  </div>
-                  <div className="button-bar">
-                  <button type="button" className="btn btn-outline-warning">
-                    <Link
-                      to={`/user/${this.state.user._id}/editHome/${home._id}`}
-                      className="btntxtclr">
-                      EDIT
+                    <Link to={`/search/${home._id}`}>
+                      <img src={home.img} alt="homepic" className="imgformat" />
                     </Link>
-                  </button>
-                  <button type="button" className="btn btn-outline-danger"
-                    onClick={this._deleteHome.bind(this, home._id)}>
-                    DELETE
-                  </button>
+                    <div className="addy">
+                      <div>{home.address.addressLine1}</div>
+                      <div>{home.address.addressLine2}</div>
+                      <div>{home.address.city}</div>
+                      <div>{home.address.state}</div>
+                      <div>{home.address.zipcode}</div>
+                      <a href={mapslink} target="_maps">
+                        View on the Googles
+                      </a>
+                    </div>
+                    <div className="button-bar">
+                      <button type="button" className="btn btn-outline-warning">
+                        <Link
+                          to={`/user/${this.state.user
+                            ._id}/editHome/${home._id}`}
+                          className="btntxtclr"
+                        >
+                          EDIT
+                        </Link>
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-outline-danger"
+                        onClick={this._deleteHome.bind(this, home._id)}
+                      >
+                        DELETE
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          })}
+              );
+            })}
+            <div className="backbutton">
+              <button type="button" class="btn btn-secondary btn-lg">
+              <Link to='/'></Link>BACK
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
   }
 }
 
